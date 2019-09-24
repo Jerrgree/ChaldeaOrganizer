99d8591650f893ebb6e1dc78af4128e07b007b3f
Jerrgree <jeremyrgreenburg@gmail.com>
2019-09-24 17:53:03 -0500

    Add new models

---
+++ ChaldeaCommon/Models/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaldeaCommon.Models
+{
+    public class Goal
+    {
+        /// <summary>
+        /// A list of items and their quantity
+        /// </summary>
+        public Dictionary<string, int> Materials { get; set; }
+        /// <summary>
+        /// A unique label for the goal
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ ChaldeaCommon/Models/Servant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaldeaCommon.Models
+{
+    class Servant
+    {
+        public string Name { get; set; }
+        public IEnumerable<Goal> Goals { get; set; }
+
+        public Servant()
+        {
+            Goals = new List<Goal>();
+        }
+    }
+}
