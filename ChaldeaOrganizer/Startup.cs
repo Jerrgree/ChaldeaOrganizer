bd51365c9e42d30b6dd6c71307a0973c6dd76754
Jerrgree <jeremyrgreenburg@gmail.com>
2019-09-27 17:58:55 -0500

    Added saving and loading data

--- ChaldeaOrganizer/Startup.cs
+++ ChaldeaOrganizer/Startup.cs
@@ -31,7 +31,7 @@ namespace ChaldeaOrganizer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<IGameDataService, GameDataService>();
+            services.AddSingleton<IGameDataService>(s => new GameDataService("Data/data.json"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- JsonGameDataService/GameDataService.cs
+++ JsonGameDataService/GameDataService.cs
@@ -1,19 +1,52 @@
 ﻿using System;
+using System.IO;
 using ChaldeaCommon.Interfaces;
 using ChaldeaCommon.Models;
+using Newtonsoft.Json;
 
 namespace JsonGameDataService
 {
     public class GameDataService : IGameDataService
     {
+        protected string fileLocation;
+        public GameDataService(string fileLocation)
+        {
+            this.fileLocation = fileLocation;
+        }
         public GameData RetrieveData()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var file = File.OpenText(fileLocation))
+                using (var reader = new JsonTextReader(file))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<GameData>(reader);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+
+                return new GameData();
+            }
         }
 
         public bool SaveData(GameData data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var file = File.CreateText(fileLocation))
+                {
+                    var serializer = new JsonSerializer();
+                    serializer.Serialize(file, data);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
