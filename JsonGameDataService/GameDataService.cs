247a03384568fb9d20cc65452f2b9c09c9bdfbaf
Jerrgree <jeremyrgreenburg@gmail.com>
2019-09-27 21:20:01 -0500

    Added some comments and handling.
    Added handling of empty files

--- JsonGameDataService/GameDataService.cs
+++ JsonGameDataService/GameDataService.cs
@@ -10,30 +10,42 @@ namespace JsonGameDataService
     public class GameDataService : IDataService<GameData>
     {
         protected string fileLocation;
+
+        /// <summary>
+        /// Initializes a new instace of the GameDataService
+        /// That is bound to a specific file for I/O
+        /// </summary>
+        /// <param name="fileLocation">The path to the file</param>
+        /// <exception cref="FileNotFoundException" />
+        /// <exception cref="ArgumentException" />
         public GameDataService(string fileLocation)
         {
-            this.fileLocation = fileLocation;
+            if (string.IsNullOrWhiteSpace(fileLocation))
+            {
+                throw new ArgumentException("fileLocation must be provided");
+            }
+            if (File.Exists(fileLocation))
+            {
+                this.fileLocation = fileLocation;
+            }
+            throw new FileNotFoundException($"{fileLocation} could not be located");
         }
+
         public Task<GameData> RetrieveData()
         {
-            try
+            using (var file = File.OpenText(fileLocation))
+            using (var reader = new JsonTextReader(file))
             {
-                using (var file = File.OpenText(fileLocation))
-                using (var reader = new JsonTextReader(file))
-                {
-                    var serializer = new JsonSerializer();
-                    return Task.FromResult(serializer.Deserialize<GameData>(reader));
-                }
-            }
-            catch (FileNotFoundException)
-            {
-                return Task.FromResult(new GameData());
+                var serializer = new JsonSerializer();
+                var gameData = serializer.Deserialize<GameData>(reader);
+                // Initialize a new object if the file was empty
+                gameData ??= new GameData();
+                return Task.FromResult(gameData);
             }
         }
 
         public Task<bool> SaveData(GameData data)
         {
-
             using (var file = File.CreateText(fileLocation))
             {
                 var serializer = new JsonSerializer();
