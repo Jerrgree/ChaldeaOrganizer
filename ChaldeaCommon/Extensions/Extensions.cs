23ab875f1ebb5546acb91227519573bb6dbcdb4b
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-01 18:10:21 -0500

    Start of new material modal

--- ChaldeaCommon/Extensions/Extensions.cs
+++ ChaldeaCommon/Extensions/Extensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ChaldeaOrganizer.Helpers
+namespace ChaldeaCommon.Extensions
 {
     public static class Extensions
     {
