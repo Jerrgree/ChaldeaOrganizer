f8b709bd3a9975f59a08bc2ac0f171c0fcca4379
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-08 20:31:52 -0500

    Refactoed JSON data service to an abstract data class

--- ChaldeaCommon/Interfaces/IDataService.cs
+++ ChaldeaCommon/Interfaces/IDataService.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace ChaldeaCommon.Interfaces
 {
-    public interface IDataService<T>
+    public interface IDataService<T> where T : new()
     {
+        /// <summary>
+        /// Retrieves stored data. If data does not exist, returns a default empty data
+        /// </summary>
+        /// <returns></returns>
         Task<T> RetrieveData();
 
+        /// <summary>
+        /// Saves data
+        /// </summary>
+        /// <param name="data">The data to save</param>
+        /// <returns></returns>
         Task<bool> SaveData(T data);
     }
 }
