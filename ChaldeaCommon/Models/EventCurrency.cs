cfd895da05b4545c579cb9e174042a58437f7073
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-08 20:03:27 -0500

    Add data models for events

---
+++ ChaldeaCommon/Models/EventCurrency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaldeaCommon.Models
+{
+    public class EventCurrency
+    {
+        public string Name { get; }
+        public string Color { get; }
+        public bool IsLotteryCurrency { get; set; }
+        public bool IsShopCurrency { get; set; }
+
+        public List<EventCurrencyShop> Shop { get; set; }
+
+        public List<int> CurrencyDropInstance { get; set; }
+
+        public EventCurrency(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
---
+++ ChaldeaCommon/Models/EventCurrencyShop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaldeaCommon.Models
+{
+    public class EventCurrencyShop
+    {
+        public int Item { get; set; }
+        public int Quantity { get; set; }
+        public int Cost { get; set; }
+    }
+}
