819cf9bd5ad855a3f2390420cf28fb54d4c82f3d
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-21 21:16:07 -0500

    Code cleanup

--- ChaldeaOrganizer/Startup.cs
+++ ChaldeaOrganizer/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ChaldeaCommon.Interfaces;
+using ChaldeaCommon.Models;
+using JsonDataServices;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ChaldeaCommon.Interfaces;
-using JsonDataServices;
-using ChaldeaCommon.Models;
 
 namespace ChaldeaOrganizer
 {
