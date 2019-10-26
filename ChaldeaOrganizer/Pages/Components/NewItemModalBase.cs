dc7dc363cd3822ecaee7ba46d3cd2140f095b7d6
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-26 10:45:49 -0500

    Moving some modal implementation to a base class

---
+++ ChaldeaOrganizer/Pages/Components/NewItemModalBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChaldeaOrganizer.Pages.Components
+{
+    public abstract class NewItemModalBase : ComponentBase
+    {
+        [Parameter]
+        public bool IsOpen { get; set; }
+
+        [Parameter]
+        public Action CloseModal { get; set; }
+
+        protected string ModalClass => IsOpen ? "show" : "";
+
+        protected abstract bool CannotAddItem { get; }
+    }
+}
