4595130c2a81eadac082f3e94336bb51d3bfe002
Jerrgree <jeremyrgreenburg@gmail.com>
2019-10-26 19:47:29 -0500

    Add currency summary

--- ChaldeaCommon/Models/EventCurrency.cs
+++ ChaldeaCommon/Models/EventCurrency.cs
@@ -23,6 +23,13 @@ namespace ChaldeaCommon.Models
 
         public int AmountNeeded => (Shop == null || Shop.Count == 0) ? 0 : Shop.Sum(x => x.Total);
 
+        public int AmountRemaining => Math.Max(AmountNeeded - AmountOwned, 0);
+
+        // Hardcode for now, make variable as a future enhancement
+        public int ApPerRun => 40;
+
+        public int RunsNeeded => (AmountRemaining > 0 && DropAverage > 0) ? (int)Math.Ceiling((decimal)AmountRemaining / DropAverage) : 0;
+
         public int AmountOwned { get; set; }
 
         public int DropAverage => (CurrencyDropInstance == null || CurrencyDropInstance.Count == 0) ? 0 : (int)Math.Floor(CurrencyDropInstance.Average());
